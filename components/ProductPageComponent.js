c4f4c434c5a628567f8a3197544ce363db9f17d8
Derek Cross <derekcrosslu@gmail.com>
2021-11-08 13:09:27 -0500

    fixed error component key

--- components/ProductPageComponent.js
+++ components/ProductPageComponent.js
@@ -1,46 +1,45 @@
-import styles from './ProductPageComponent.module.css'
+import styles from "./ProductPageComponent.module.css";
 const imageUrl = "https://www.jcrew.com/s7-img-facade/";
-import Image from 'next/image'
-
+import Image from "next/image";
 
 function ProductPageComponent(props) {
-      const {
-        colors,
-        defaultColorCode,
-        productCode,
-        productDescription,
-        productId,
-        listPrice,
-        imageSrc = imageUrl + productCode + "_" + defaultColorCode + "_m",
-      } = props.product_selected;
-  
-        const colorUrl = (color) =>
-          imageUrl + productCode + "_" + color.colorCode + "_sw";
-      
-    return (
-      <section >
-        <Image src={imageSrc} width="700" height="700" />
-        <div className={styles.content}>
-          <h3> {productDescription}</h3>
-          <p>{listPrice.formatted}</p>
-          <div className={styles.swatches}>
-            {colors.map((color) => {
-              return (
-                <div>
-                  {console.log(colorUrl(color))}
-                  <Image
-                    src={colorUrl(color)}
-                    width="25"
-                    height="25"
-                    className={styles.circle}
-                  />
-                </div>
-              );
-            })}
-          </div>
+  const {
+    colors,
+    defaultColorCode,
+    productCode,
+    productDescription,
+    productId,
+    listPrice,
+    imageSrc = imageUrl + productCode + "_" + defaultColorCode + "_m",
+  } = props.product_selected;
+
+  const colorUrl = (color) =>
+    imageUrl + productCode + "_" + color.colorCode + "_sw";
+
+  return (
+    <section>
+      <Image src={imageSrc} width="700" height="700" />
+      <div className={styles.content}>
+        <h3> {productDescription}</h3>
+        <p>{listPrice.formatted}</p>
+        <div className={styles.swatches}>
+          {colors.map((color, index) => {
+            return (
+              <div>
+                <Image
+                  src={colorUrl(color)}
+                  width="25"
+                  height="25"
+                  className={styles.circle}
+                  key={index}
+                />
+              </div>
+            );
+          })}
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 }
 
 export default ProductPageComponent;
